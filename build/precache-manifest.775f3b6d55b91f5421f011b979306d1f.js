0c74c56637f729c6c564f3ce65c9966892853818
Evan Bassler <evan@cuberis.com>
2019-07-05 20:47:26 -0400

    update homepage

---
+++ build/precache-manifest.775f3b6d55b91f5421f011b979306d1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "eee8ed1315cb72c7aecee2ad92a04bd0",
+    "url": "/static/media/bg.eee8ed13.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "31252da89b2ec9e47016",
+    "url": "/static/js/main.31252da8.chunk.js"
+  },
+  {
+    "revision": "11ecf0399f08d254addb",
+    "url": "/static/js/1.11ecf039.chunk.js"
+  },
+  {
+    "revision": "31252da89b2ec9e47016",
+    "url": "/static/css/main.149d1019.chunk.css"
+  },
+  {
+    "revision": "f22baf756599de056721e6ed02a5e97b",
+    "url": "/index.html"
+  }
+];
